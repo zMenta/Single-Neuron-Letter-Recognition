46ecc5622403b877ee1a81ab1d4ac1dca29bee8c
LucasBrentano <brentano.l@gmail.com>
2022-09-09 17:17:35 -0300

    Basic training

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from Neuron.Neuron import Neuron
+from image_processing import convert_image
 import single_neuron_network as network
 
-neuron = Neuron(2)
+neuron = Neuron(400)
 
 
-# Testing Data
-input1 = [1, 1]
-input2 = [1, 0]
-input3 = [0, 1]
-input4 = [0, 0]
+#  Testing Data
+# input1 = [1, 1]
+# input2 = [1, 0]
+# input3 = [0, 1]
+# input4 = [0, 0]
 
-# [Input list, desired output]
+#   [Input list, desired output]
+# data_set = [
+#     [input1, 1],
+#     [input2, 1],
+#     [input3, 0],
+#     [input4, 0],
+# ]
+
+
+# Image testing
+
+# Downscale value
+img_px = 20
+
 data_set = [
-    [input1, 1],
-    [input2, 1],
-    [input3, 0],
-    [input4, 0],
+    [convert_image("TrainingData/LetterA/a.jpg", img_px), 1],
+    [convert_image("TrainingData/LetterB/b.jpg", img_px), 0]
 ]
 
 
