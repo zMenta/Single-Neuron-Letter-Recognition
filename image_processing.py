be2ea8723e8f7907bb8ab03b6fe1179e1510bdb5
LucasBrentano <brentano.l@gmail.com>
2022-09-09 14:54:18 -0300

    Image Processing testing

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,9 @@
+from audioop import avg
 from PIL import Image
+
+
+def average(tuple_data: tuple) -> float:
+    return sum(tuple_data) / len(tuple_data)
 
 
 im = Image.open("godot_image")
@@ -9,8 +14,10 @@ processed_image = im.resize((20, 20))
 
 image_data = list(processed_image.getdata())
 
-print(image_data[0])
+print(image_data[10])
 
 print(len(image_data))
+
+print(average(image_data[10]))
 
 # print(processed_image.histogram())
