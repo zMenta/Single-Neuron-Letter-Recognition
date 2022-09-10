ea6396f7a9f3915d77f7934d0b998243537a4806
Lucas Brentano <brentano.l@gmail.com>
2022-09-09 21:25:22 -0300

    WIP | Working data_process. PNG is the bug?

--- data_processing.py
+++ data_processing.py
@@ -4,21 +4,12 @@ from image_processing import convert_image
 data_path = "Data/TrainingData/LetterA/"
 data_list = []
 desired_output = 1
-pixel_density = 2
+pixel_density = 40
 
-file_number = 1
+
 for file in os.listdir(data_path):
     # file is str
-    # print(file)
     img_list = convert_image(data_path + file, pixel_density)
     data_set = [img_list, desired_output]
 
     data_list.append(data_set)
-
-    if file_number == 5:
-        break
-
-    file_number += 1
-
-# print(data_list)
-print(file_number)
